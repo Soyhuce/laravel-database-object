6ab3e3ae20ddcab04c3cdff99edceec490e10d63
Bastien Philippe <bastien.philippe@soyhuce.fr>
2023-12-18 08:45:16 +0100

    wip

--- src/DatabaseObject.php
+++ src/DatabaseObject.php
@@ -2,15 +2,33 @@
 
 namespace Soyhuce\DatabaseObject;
 
-abstract class DatabaseObject
+use JsonSerializable;
+
+abstract class DatabaseObject implements JsonSerializable
 {
     /**
      * @param array<string, mixed>  $data
      */
-    abstract public static function fromDatabase(array $data): static;
+    abstract public static function create(array $data): static;
 
     /**
      * @return array<string, mixed>
      */
-    abstract public function toDatabase(): array;
+    abstract public function jsonSerialize(): array;
+
+    /**
+     * @param array<string, mixed>  $data
+     */
+    public static function fromDatabase(array $data): static
+    {
+        return static::create($data);
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function toDatabase(): array
+    {
+        return $this->jsonSerialize();
+    }
 }
